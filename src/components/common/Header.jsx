6847922ee517b00c374fd83b4b90cac4d7a1f626
Alexandre BOYER <alexandre.boyer29@gmail.com>
2026-04-12 22:52:02 +0200

    style: improve Header
    - add icon and link
    - better spacing

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,23 +3,31 @@ import ThemeControllerSwap from "./ThemeControllerSwap.jsx";
 
 function Header() {
     return (
-        <div className="navbar bg-base-100 shadow-sm">
-            <div className="flex-none">
-                <a className="btn btn-ghost text-xl">al.capitan</a>
+        <div className="navbar bg-base-100 shadow-sm px-8">
+            <div className="flex-1 gap-2">
+                <a href="/" className="flex items-center gap-4 group">
+                    <img 
+                        src="/public/favicon.png" 
+                        alt="Logo" 
+                        className="w-10 h-10"
+                    />
+                <span className="text-xl font-bold">
+                    alcapitan.me
+                </span>
+                </a>
             </div>
-            <div className="flex-1"></div>
-            <div className="flex-none">
+            <div className="flex-none gap-4">
                 <ul className="menu menu-horizontal px-1">
-                <li><a>Link</a></li>
-                <li>
-                    <details>
-                    <summary>Parent</summary>
-                    <ul className="bg-base-100 rounded-t-none p-2">
-                        <li><a>Link 1</a></li>
-                        <li><a>Link 2</a></li>
-                    </ul>
-                    </details>
-                </li>
+                    <li><a>Link</a></li>
+                    <li>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul className="bg-base-100 rounded-t-none p-2">
+                                <li><a>Link 1</a></li>
+                                <li><a>Link 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
                 </ul>
                 {/* <ThemeControllerDropdown /> */}
                 <ThemeControllerSwap />
