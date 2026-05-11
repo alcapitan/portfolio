147014ee69bf204f563d994e3f4443ceed0786d5
Al Capitan <alexandre.boyer29@gmail.com>
2026-05-11 18:18:33 +0200

    feat: remove initial content and set a bit of mine

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
             <div className="flex-1 gap-2">
                 <a href="/" className="flex items-center gap-4 group">
                     <img 
-                        src="/public/favicon.png" 
+                        src="/favicon.png" 
                         alt="Logo" 
                         className="w-10 h-10"
                     />
@@ -17,17 +17,10 @@ function Header() {
                 </a>
             </div>
             <div className="flex-none gap-4">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
+                <ul className="menu menu-horizontal px-1 mr-4">
+                    <li><a href="#about">À propos</a></li>
+                    <li><a href="#projects">Mes projets</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
                 {/* <ThemeControllerDropdown /> */}
                 <ThemeControllerSwap />
